32a19fb5beb961b199a0a291b9550efb4e770f72
lillehau <lillehau@384eb081-3c5d-4ce6-94eb-64c770dda9dc>
2010-09-01 18:06:12 +0000

    Pluginclasses,
    git-svn-id: https://underdusken.no:88/svn/intern/annonseweb@155 384eb081-3c5d-4ce6-94eb-64c770dda9dc

---
+++ api/src/main/java/no/dusken/annonseweb/plugin/AnnonsewebPlugin.java
@@ -0,0 +1,12 @@
+package no.dusken.annonseweb.plugin;
+
+import no.dusken.common.plugin.DuskenPluginAdapter;
+
+/**
+ * @author Marvin B. Lillehaug <lillehau@underdusken.no>
+ */
+public class AnnonsewebPlugin extends DuskenPluginAdapter {
+    protected AnnonsewebPlugin(String pluginUid) {
+        super(pluginUid);
+    }
+}
---
+++ api/src/main/java/no/dusken/annonseweb/plugin/AnnonsewebServices.java
@@ -0,0 +1,9 @@
+package no.dusken.annonseweb.plugin;
+
+import org.kantega.jexmec.Services;
+
+/**
+ * @author Marvin B. Lillehaug <lillehau@underdusken.no>
+ */
+public interface AnnonsewebServices extends Services{
+}
