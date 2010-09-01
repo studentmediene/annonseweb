91521164d52020c90833435c8ca440f20794c846
lillehau <lillehau@384eb081-3c5d-4ce6-94eb-64c770dda9dc>
2010-09-01 18:19:09 +0000

    runs.
    git-svn-id: https://underdusken.no:88/svn/intern/annonseweb@156 384eb081-3c5d-4ce6-94eb-64c770dda9dc

--- api/src/main/java/no/dusken/annonseweb/plugin/AnnonsewebServices.java
+++ api/src/main/java/no/dusken/annonseweb/plugin/AnnonsewebServices.java
@@ -1,9 +1,25 @@
 package no.dusken.annonseweb.plugin;
 
+import no.dusken.common.dao.GenericDao;
+import no.dusken.common.service.MailService;
 import org.kantega.jexmec.Services;
+import org.kantega.jexmec.store.PluginStoreProvider;
 
 /**
  * @author Marvin B. Lillehaug <lillehau@underdusken.no>
  */
 public interface AnnonsewebServices extends Services{
+
+    /**
+     * Provides access to persistent state so the plugin can store configuration
+     * etc.
+     * @return the PluginStoreProvider
+     */
+    PluginStoreProvider getPluginStoreProvider();
+
+    /**
+     * Provides access to the generic dao that can handle <T extends AraneaObject>
+     * @return the {@link GenericDao} dao
+     */
+    GenericDao getGenericDao();
 }
