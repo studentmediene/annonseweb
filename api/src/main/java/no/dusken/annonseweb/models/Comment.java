5ee556678e8fad32bc083d466088fa08724e133d
Emilie Ek <emiliebr@stud.ntnu.no>
2011-10-06 18:45:26 +0200

    Added variables to the customer class and implemented getters and setters. Created new class, Comment, and added the variables.

--- api/src/main/java/no/dusken/annonseweb/models/Comment.java
+++ api/src/main/java/no/dusken/annonseweb/models/Comment.java
@@ -1,20 +1,53 @@
 package no.dusken.annonseweb.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Emilie2
- * Date: 05.10.11
- * Time: 19:18
- * To change this template use File | Settings | File Templates.
- */
 public class Comment {
+
+    @Id
+    @GeneratedValue
     long ID;
+
     String text;
 
     Date created_date;
     String created_user;
     Date lastedited_date;
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public String getCreated_user() {
+        return created_user;
+    }
+
+    public Date getLastedited_date() {
+        return lastedited_date;
+    }
+
+    public String getLastedited_user() {
+        return lastedited_user;
+    }
+
+    public String getText() {
+
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
     String lastedited_user;
 }
---
+++ api/src/main/java/no/dusken/annonseweb/models/ContactPerson.java
@@ -0,0 +1,92 @@
+package no.dusken.annonseweb.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+public class ContactPerson {
+    @Id
+    @GeneratedValue
+    private long ID;
+
+    private String person_name;
+    private String email;
+    private String telephone_number;
+    private String company_position;
+
+    private Customer customer;
+
+    private Date last_contacted_time;
+    private Date last_contacted_user;
+
+    private Boolean active;
+
+
+    public ContactPerson(String person_name, String email, String telephone_number, String company_position, Customer customer) {
+        this.person_name = person_name;
+        this.email = email;
+        this.telephone_number = telephone_number;
+        this.company_position = company_position;
+        this.customer = customer;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public Date getLast_contacted_time() {
+        return last_contacted_time;
+    }
+
+    public Date getLast_contacted_user() {
+        return last_contacted_user;
+    }
+
+    public String getPerson_name() {
+        return person_name;
+    }
+
+    public void setPerson_name(String person_name) {
+        this.person_name = person_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone_number() {
+        return telephone_number;
+    }
+
+    public void setTelephone_number(String telephone_number) {
+        this.telephone_number = telephone_number;
+    }
+
+    public String getCompany_position() {
+        return company_position;
+    }
+
+    public void setCompany_position(String company_position) {
+        this.company_position = company_position;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
