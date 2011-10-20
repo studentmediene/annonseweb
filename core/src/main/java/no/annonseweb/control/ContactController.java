8b2cf79cdb1836bc04eaec7aa5dd9a5532d56580
Emilie Ek <emiliebr@stud.ntnu.no>
2011-10-20 20:36:09 +0200

    Added more views and modified sidebar.

--- core/src/main/java/no/annonseweb/control/ContactController.java
+++ core/src/main/java/no/annonseweb/control/ContactController.java
@@ -11,15 +11,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Time: 19:32
  */
 @Controller
+@RequestMapping("/contacts")
 public class ContactController {
+
+
+
+    @RequestMapping("/contactsHome")
+    public String viewContactsHome(){
+        return "no/dusken/annonseweb/web/contacts/contactHome";
+    }
 
     @RequestMapping("/viewContacts")
     public String viewContacts(){
         return "no/dusken/annonseweb/web/contacts/viewContact";
     }
 
-    @RequestMapping("/contactsHome")
-    public String viewContactsHome(){
-        return "no/dusken/annonseweb/web/contacts/contactHome";
+    @RequestMapping("/newContact")
+    public String viewNewContacts(){
+        return "no/dusken/annonseweb/web/contacts/newContact";
+    }
+
+    @RequestMapping("/newContactPerson")
+    public String viewNewContactPerson(){
+        return "no/dusken/annonseweb/web/contacts/newContactPerson";
+    }
+
+    @RequestMapping("/searchContact")
+    public String viewSearchContacts(){
+        return "no/dusken/annonseweb/web/contacts/searchContact";
+    }
+
+    @RequestMapping("/viewContactPersons")
+    public String viewContactPersons(){
+        return "no/dusken/annonseweb/web/contacts/viewContactPersons";
     }
 }
