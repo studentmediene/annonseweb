271a8d31616e275b3d2ab2624d35df9ee785e0b9
Marvin B. Lillehaug <marv@Jane.(none)>
2011-11-10 21:50:05 +0100

    Works, but handler mappings are fucked. The controllers do not have .do, but the rest of config is set up as if they do have .do.

--- core/src/main/java/no/dusken/annonseweb/control/SidebarController.java
+++ core/src/main/java/no/dusken/annonseweb/control/SidebarController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SidebarController {
 
-    @RequestMapping("/sidebar")
+    @RequestMapping("/sidebar.do")
     public String viewSidebar(){
         return "sidebar";
     }
