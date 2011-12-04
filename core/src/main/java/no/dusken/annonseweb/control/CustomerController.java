af5b5e8360842939d00716cbb43663ce110b9957
magnuskiro <magnuskiro@gmail.com>
2011-12-04 22:10:54 +0100

    Fixed parsing of sidebar in views. This has to be changed for all the other view groups as well. Coded some more on customer controller, now the creation of customers is working. But the input has to be sanitized and the UI completed. The customerList works as it should, but the UI has to be finished.

--- core/src/main/java/no/dusken/annonseweb/control/CustomerController.java
+++ core/src/main/java/no/dusken/annonseweb/control/CustomerController.java
@@ -5,6 +5,7 @@ import no.dusken.annonseweb.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,14 +25,10 @@ public class CustomerController{
         return "customers/home";
     }
 
-    @RequestMapping("/new")
-    public String viewNewCustomer(){
-        return "customers/new";
-    }
-
-    @RequestMapping("/customer")
-    public String viewCustomer(@RequestParam Long id, Model model){
-        Customer customer = customerService.findOne(id);
+    @RequestMapping(value="/customer/{Id}")
+    public String viewCustomer(@PathVariable Long Id, Model model){
+        Customer customer = customerService.findOne(Id);
+        customer.getId();
         model.addAttribute("customer", customer);
         return "customers/customer";
     }
@@ -39,18 +36,23 @@ public class CustomerController{
     @RequestMapping("/all")
     public String viewCustomerList(Model model){
         List<Customer> customerList = customerService.findAll();
-        /* only for testing
-        List<Customer> customerList = new ArrayList<Customer>();
-        BigInteger num = new BigInteger(String.valueOf(10000));
-        List<String> tags = new ArrayList<String>();
-        tags.add("kake");
-        tags.add("elefant");
-        customerList.add(new Customer("test1", "email", "tlf", "invoicead", "subscadr", num, tags));
-        customerList.add(new Customer("test2", "email", "tlf", "invoicead2", "subscadr2", num, tags));
-        customerList.add(new Customer("test3", "email", "tlf", "invoicead3", "subscadr3", num, tags));
-        */
         model.addAttribute("customerList", customerList);
         return "customers/all";
+    }
+
+    @RequestMapping("/new")
+    public String viewNewCustomer(){
+        return "customers/new";
+    }
+
+    @RequestMapping("/addCustomer")
+    public String addCustomer(@RequestParam String customerName, @RequestParam String centralEmail,
+                              @RequestParam String centralTlf, @RequestParam String invoiceAddress,
+                              @RequestParam String subscriberAddress, @RequestParam BigInteger discount,
+                              @RequestParam List<String> industryTags, Model model){
+        Customer customer = new Customer(customerName, centralEmail, centralTlf, invoiceAddress, subscriberAddress, discount, industryTags);
+        customerService.save(customer);
+        return "customers/customer/" + customer.getId();
     }
 
     @RequestMapping("/search")
