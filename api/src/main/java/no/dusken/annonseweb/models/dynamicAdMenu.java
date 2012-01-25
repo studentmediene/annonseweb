e2e059378d4d661b6e5300c0e22aa3e4c36cb70b
Stine <stine@sheldon.(none)>
2012-01-25 22:06:42 +0100

    Lagt til knapp for å adde nye annonser i ny annonse

---
+++ api/src/main/java/no/dusken/annonseweb/models/dynamicAdMenu.java
@@ -0,0 +1,14 @@
+package no.dusken.annonseweb.models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stine
+ * Date: 1/19/12
+ * Time: 7:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class dynamicAdMenu {
+    int value;
+
+
+}
