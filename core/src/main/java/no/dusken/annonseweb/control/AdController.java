cb49d7712aa6e36bf8265a955247eba72e9ad385
Emilie Ek <emiliebr@stud.ntnu.no>
2012-03-28 22:26:54 +0200

    Updated "new contactnote" and the sale-links which directed to the folder "sales" which was called "sale". (datasource.properties updated to make the newest pull compile)

--- core/src/main/java/no/dusken/annonseweb/control/AdController.java
+++ core/src/main/java/no/dusken/annonseweb/control/AdController.java
@@ -1,6 +1,7 @@
 package no.dusken.annonseweb.control;
 
 import no.dusken.annonseweb.models.Ad;
+import no.dusken.annonseweb.models.WebAd;
 import no.dusken.annonseweb.service.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,7 +27,7 @@ public class AdController {
 
     @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String viewNewAdGeneral(Model model){
-        return viewAdHome(new Ad(), model);
+        return viewAdHome(new WebAd(), model);           //Get type of Ad and initialize the correct one
     }
 
     @RequestMapping(value = "/new", method = RequestMethod.POST)
