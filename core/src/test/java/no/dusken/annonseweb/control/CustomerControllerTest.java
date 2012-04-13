78cd2857fc6fc87580c0d95b505976da18026779
Magnus Kirø <magnuskiro@gmail.com>
2012-04-13 11:37:52 +0200

    done a lot of report improvements. Mostly added todo elements.

---
+++ core/src/test/java/no/dusken/annonseweb/control/CustomerControllerTest.java
@@ -0,0 +1,48 @@
+package no.dusken.annonseweb.control;
+
+import org.junit.Test;
+
+/**
+ * @author Magnus Kirø - magnuskiro@ gmail.com/underdusken.no - 12/04/12
+ */
+public class CustomerControllerTest {
+    @Test
+    public void testViewCustomerHome() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testViewCustomer() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testViewCustomerList() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testViewNewCustomer() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testViewEmailsCustomersMain() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testViewEdit() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testEdit() throws Exception {
+        // TODO:
+    }
+
+    @Test
+    public void testInitbinder() throws Exception {
+        // TODO:
+    }
+}
