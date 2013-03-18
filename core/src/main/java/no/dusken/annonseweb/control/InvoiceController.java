c3bde5f8579f263777f7b562121aef0e1aaebd70
Magnus Kirø <magnuskiro@gmail.com>
2013-03-18 17:52:09 +0100

    added todo fields, and fingured out that it wont run with chrome. this should be further investigated

--- core/src/main/java/no/dusken/annonseweb/control/InvoiceController.java
+++ core/src/main/java/no/dusken/annonseweb/control/InvoiceController.java
@@ -38,6 +38,7 @@ public class InvoiceController {
 
     @RequestMapping("/{invoice}")
     public String viewInvoice(@PathVariable Invoice invoice, Model model){
+        // todo does this work?
         model.addAttribute("invoice", invoice);
         return "invoice/invoice";
     }
@@ -62,6 +63,7 @@ public class InvoiceController {
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveNewInvoice(@Valid @ModelAttribute Invoice invoice) {
+        // todo get getLoggedInUser, ain't that in commons ?
         invoice.setCreatedUser(annonsePersonController.getLoggedInUser());
         invoice.setLastEditedUser(annonsePersonController.getLoggedInUser());
         invoice.setCreatedDate(Calendar.getInstance());
