92c0aeb460670b0a855c707cf56ad1b44362f09a
jorhol <holmefjord@gmail.com>
2013-04-27 15:55:40 +0200

    Users can now register radio ads

---
+++ api/src/main/java/no/dusken/annonseweb/models/RadioAd.java
@@ -0,0 +1,188 @@
+package no.dusken.annonseweb.models;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.math.BigDecimal;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+public class RadioAd extends Ad {
+
+    public enum AdFormat {
+        PROGRAM("Radio",10000),     // Beskrivelse , pris
+        PODCAST("Podcast",10000);
+        private final Integer price;
+        private final String description;
+
+        AdFormat(String desc, Integer value){
+            price = value;
+            description = desc;
+        }
+
+        public Integer getPrice(){
+            return price;
+        }
+
+        public String getDescription(){
+            return description;
+        }
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar onlineFrom = Calendar.getInstance();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar onlineTo = Calendar.getInstance();
+
+    private AdFormat adFormat;      //Updates price accordingly, or manually when created
+
+    private transient String editNumber;
+
+    public RadioAd(){}
+
+    public RadioAd(Sale sale, BigDecimal price, BigDecimal discount, Boolean active, Boolean placedOnProd,
+                 AnnonsePerson createdUser, Integer adFormat, String onlineFrom) {
+        super(sale, price, discount, active, placedOnProd, createdUser);
+        this.setAdFormat(adFormat);
+        this.setOnlineFrom(onlineFrom);
+    }
+
+    public void cloneFrom(RadioAd other) {
+        if (other == null){
+            return;
+        }
+        this.setPrice(other.getPrice());
+        this.setDiscount(other.getDiscount());
+        this.setActive(other.getActive());
+        this.setSale(other.getSale());
+        this.setLastEditedDate();
+        this.adFormat = other.adFormat;
+        this.onlineFrom = other.onlineFrom;
+        this.onlineTo = other.onlineTo;
+        this.setPlacedOnProd(other.getPlacedOnProd());
+
+    }
+
+    public void cloneFrom(PrintedAd other) {}
+    public void cloneFrom(WebAd other) {}
+
+    public String getOnlineFrom() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String strdate = null;
+        if (this.onlineFrom != null) {
+            strdate = sdf.format(this.onlineFrom.getTime());
+        }
+        return strdate;
+    }
+
+    public void setOnlineFrom(String onlineFrom) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.onlineFrom.setTime(sdf.parse(onlineFrom));
+            setOnlineTo(onlineFrom);
+        } catch (Exception e) {}
+    }
+
+    public void setOnlineTo(String onlineFrom) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.onlineTo.setTime(sdf.parse(onlineFrom));
+            if(this.adFormat == AdFormat.PROGRAM){
+                this.onlineTo.add(Calendar.DATE,7);
+            }
+            else{
+                this.onlineTo.add(Calendar.DATE, 10000);
+            }
+        } catch (Exception e) {}
+    }
+
+    public String getOnlinePeriod() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String strdate = null;
+        if (this.onlineFrom != null && this.onlineTo != null) {
+            strdate = sdf.format(this.onlineFrom.getTime());
+            strdate += " - ";
+            strdate += sdf.format(this.onlineTo.getTime());
+        }
+        return strdate;
+    }
+
+    public Integer getAdFormat() {
+        if(this.adFormat == AdFormat.PROGRAM) { return 1;}
+        else if(this.adFormat == AdFormat.PODCAST) {return 2;}
+        else {return 0;}
+    }
+
+    public void setAdFormat(Integer webFormat) {
+        switch (webFormat) {
+            case 1: this.adFormat = AdFormat.PROGRAM;
+                break;
+            case 2: this.adFormat = AdFormat.PODCAST;
+                break;
+        }
+    }
+
+    public String getAdDescription() {
+        return this.adFormat.getDescription();
+    }
+
+    public Map<Integer,String> getAdFormatList(){
+        Map<Integer,String> adFormatList = new HashMap<Integer,String>();
+        Integer i = 1;
+        for(AdFormat formats : adFormat.values()){
+            adFormatList.put(i,formats.getDescription());
+            i++;
+        }
+        return adFormatList;
+    }
+
+    public List<Integer> getPriceByAdFormat(){
+        List<Integer> prices = new ArrayList<Integer>();
+        Integer i = 1;
+        for(AdFormat formats : adFormat.values()){
+            prices.add(formats.getPrice());
+            i++;
+        }
+        return prices;
+    }
+
+    public String getEditNumber() {
+        return editNumber;
+    }
+
+    public void setEditNumber(String editNumber) {
+        try {
+            Long.valueOf(editNumber);
+            this.editNumber = editNumber;
+        } catch (NumberFormatException e) {}
+    }
+
+    public Integer getFromDay() {
+        return this.onlineFrom.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public Integer getFromMonth() {
+        return this.onlineFrom.get(Calendar.MONTH) + 1;
+    }
+
+    public Integer getFromYear() {
+        return this.onlineFrom.get(Calendar.YEAR);
+    }
+
+    public Integer getToDay() {
+        return this.onlineTo.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public Integer getToMonth() {
+        return this.onlineTo.get(Calendar.MONTH) + 1;
+    }
+
+    public Integer getToYear() {
+        return this.onlineTo.get(Calendar.YEAR);
+    }
+}
